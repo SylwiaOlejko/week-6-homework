48089e02c9b3ef4f93d9724588df140018327c80
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-02-22 21:31:15 +0100

    api url

--- src/index.js
+++ src/index.js
@@ -41,6 +41,9 @@ function formatDate(date) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeatherCondition);
+
+    apiUrl = '`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric';
+    axios.get(apiUrl).then(displayForecast);
   }
   
   function handleSubmit(event) {
