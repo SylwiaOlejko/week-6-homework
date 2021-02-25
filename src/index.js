01928a892ce6298ca37f239d48cceddd611e892f
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-02-25 22:58:06 +0100

    null

--- src/index.js
+++ src/index.js
@@ -70,10 +70,13 @@ function formatDate(date) {
   
   function dispalyForecast(response) {
     let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = null;
+
     let forecast = null;
+    
    for (let index = 0; index < 6; index++){
       forecast = responce.data.list[index];
-      forecastElement.innerHTML = `
+      forecastElement.innerHTML += `
       <div class="col-2">
         <h3>
           ${formatHours(forecast.dt * 1000)}
